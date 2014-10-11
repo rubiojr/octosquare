68cc618561cfd4dd1f9f670bf2870db9575cf08d
Sergio Rubio <rubiojr@frameos.org>
2014-10-12 00:56:49 +0200

    Initial release

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"github.com/grindhold/gominatim"
 	"github.com/op/go-logging"
+	"github.com/rubiojr/freegeoip-client"
+	"github.com/rubiojr/kingpin"
 	"os"
 	"strings"
 )
@@ -12,9 +14,23 @@ import (
 var log = logging.MustGetLogger("places")
 var format = "%{level} ▶ %{message}"
 
-func main() {
-	logging.SetFormatter(logging.MustStringFormatter(format))
+var (
+	debug = kingpin.Flag("debug", "Enable debug mode.").Bool()
 
+	setup     = kingpin.Command("setup", "Setup wizard.")
+	searchCmd = kingpin.Command("search", "Search for a place.")
+	//postImage   = post.Flag("image", "Image to post.").File()
+	searchPlace = searchCmd.Arg("place", "Place name.").Required().Strings()
+
+	addCmd        = kingpin.Command("add", "Add a place.")
+	addCmdPlace   = addCmd.Arg("place", "Place name.").Required().String()
+	addCurrentCmd = kingpin.Command("add-current",
+		"Add your current position.")
+
+	locateMeCmd = kingpin.Command("locate-me", "Print my location.")
+)
+
+func search(args string) []map[string]string {
 	gominatim.SetServer("https://nominatim.openstreetmap.org/")
 
 	if len(os.Args) < 2 {
@@ -22,7 +38,42 @@ func main() {
 		os.Exit(1)
 
 	}
-	args := strings.Join(os.Args[1:], " ")
+
+	//Get by a Querystring
+	log.Info("Searching for '" + args + "'...")
+	qry := gominatim.SearchQuery{
+		Q: args,
+	}
+	resp, _ := qry.Get() // Returns []gominatim.Result
+	var locs []map[string]string
+	if len(resp) > 0 {
+		log.Info("Found locations:\n")
+		for _, loc := range resp {
+			p := map[string]string{
+				"name": loc.DisplayName,
+				"lat":  loc.Lat,
+				"lon":  loc.Lon,
+			}
+			locs = append(locs, p)
+			log.Info("  -> %s (%s, %s)\n",
+				loc.DisplayName, loc.Lat, loc.Lon)
+		}
+	} else {
+		log.Error("No locations found for: '%s'", args)
+
+	}
+
+	return locs
+}
+
+func add(args string) {
+	gominatim.SetServer("https://nominatim.openstreetmap.org/")
+
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: octosquare <location>")
+		os.Exit(1)
+
+	}
 
 	//Get by a Querystring
 	log.Info("Searching for '" + args + "'...")
@@ -56,4 +107,31 @@ func main() {
 		panic("Error opening places file for writing")
 	}
 	file.Write(str)
+
+}
+
+func main() {
+
+	switch kingpin.Parse() {
+	// Register user
+	case "setup":
+		println("TODO")
+	case "add":
+		add(*addCmdPlace)
+	case "add-current":
+		loc, err := freegeoip_client.GetLocation()
+		if err != nil {
+			fmt.Println("Error getting my location.")
+		}
+		add(loc.CountryName + ", " + loc.City)
+	case "search":
+		search(strings.Join(*searchPlace, " "))
+	case "locate-me":
+		loc, err := freegeoip_client.GetLocation()
+		if err != nil {
+			fmt.Println("Error getting my location.")
+		}
+		fmt.Printf("%s, %s (%f, %f)", loc.CountryName,
+			loc.City, loc.Longitude, loc.Latitude)
+	}
 }
